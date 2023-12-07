6e9a3dbb2b42d3888b489a1d564d392f7196ff2b
ky302 <ahmadsubekhi550@gmail.com>
2023-12-07 14:24:35 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 // import './App.css';
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Login from './components/Login';
-import Konten from './components/Konten';
-import RegisrtrationForm from './components/RegisrtrationForm';
+import Login from "./components/Login";
+import Konten from "./components/Konten";
+import RegisrtrationForm from "./components/RegisrtrationForm";
 import Macth from "./components/Macth";
 
 function App() {
   return (
     <>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboardKonten" element={<Konten />} />
-        <Route path="/formRegistration" element={<RegisrtrationForm />} />
-        <Route path="/dataMatch" element={<Macth />} />
-      </Routes>
-    </HashRouter>
-  </>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboardKonten" element={<Konten />} />
+          <Route path="/formRegistration" element={<RegisrtrationForm />} />
+          <Route path="/dataMatch" element={<Macth />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
